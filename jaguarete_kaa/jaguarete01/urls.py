0844e91f2a875e06af6929bc64e25319311df13c
dmoreira317 <dmoreiraventas@gmail.com>
2021-06-15 22:16:56 -0300

    Added first view, configured app urls and project urls

---
+++ jaguarete_kaa/jaguarete01/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from django.urls import path
+from jaguarete01 import views
+from django.contrib.auth import views as auth_views
+
+app_name = 'jaguarete01'
+
+urlpatterns = [
+    # marvelapp pages
+    path("index/", views.index, name = "index"),
+]
--- jaguarete_kaa/jaguarete_kaa/settings.py
+++ jaguarete_kaa/jaguarete_kaa/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'jaguarete01',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'jaguarete_kaa.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['jaguarete01/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
