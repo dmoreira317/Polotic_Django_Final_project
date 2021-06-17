3f79e8b5cd654f0c4daf2acf5d15861381d059ea
dmoreira317 <dmoreiraventas@gmail.com>
2021-06-17 11:35:43 -0300

    Added Producto model and registered it

--- jaguarete_kaa/jaguarete01/admin.py
+++ jaguarete_kaa/jaguarete01/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from jaguarete01.models import Producto
 
 # Register your models here.
+admin.site.register(Producto)
--- jaguarete_kaa/jaguarete01/models.py
+++ jaguarete_kaa/jaguarete01/models.py
@@ -2,4 +2,13 @@ from django.db import models
 
 # Create your models here.
 
+class Producto(models.Model):
+    titulo = models.CharField(max_length=40, unique=True)
+    categoria = models.CharField(max_length=40)
+    precio = models.FloatField()
+    imagen = models.FileField()
+    fecha_creacion = models.DateTimeField()
+    
+
+
 
