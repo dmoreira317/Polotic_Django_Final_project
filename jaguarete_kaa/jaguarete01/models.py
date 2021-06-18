2cdd3bedd7ed5ed4c96748b5ed7653d7429ff0ec
Diego M <77156529+dmoreira317@users.noreply.github.com>
2021-06-17 22:11:43 -0300

    Added a base js file for cart, hidthe django secret_key

--- jaguarete_kaa/jaguarete01/models.py
+++ jaguarete_kaa/jaguarete01/models.py
@@ -2,13 +2,19 @@ from django.db import models
 
 # Create your models here.
 
+class Categoria(models.Model):
+    descripcion = models.CharField(max_length=60)
+
 class Producto(models.Model):
     titulo = models.CharField(max_length=40, unique=True)
-    categoria = models.CharField(max_length=40)
+    # categoria = models.ForeignKey()
     precio = models.FloatField()
     imagen = models.FileField()
     fecha_creacion = models.DateTimeField()
+
+class Carrito():
+    usuario = models.CharField()
+    lista_productos = models.CharField()
+    precio_total = models.FloatField()
+
     
-
-
-
--- jaguarete_kaa/jaguarete_kaa/settings.py
+++ jaguarete_kaa/jaguarete_kaa/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +27,7 @@ print(STATIC_DIR)
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gi%#=q$z579k+9%fm27d3v^25oe!a%o*gouslmebifklm&9ecd'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
